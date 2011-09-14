3b4d435ac1815ebf7e60bb8bbd9226131608d746
Ed van Beinum <ed@vanbeinum.org>
2011-09-14 06:59:18 +0100

    remove alpha to start afresh on beta version

--- tests/Husky/HuskyTest.php
+++ tests/Husky/HuskyTest.php
@@ -25,35 +25,7 @@ class HuskyTest extends PHPUnit_Framework_TestCase {
         $this->_husky = new \Husky\Husky;
     }
 
-    /**
-     * Are the member vars set with the expected vales form config when Husky class is onstantiated?
-     *
-     * @test
-     * @return void
-     */
-    public function construct_sets_member_vars_from_config()
-    {
-       // $this->assertSame($this->_husky->getParser()->g, \Husky\Config::PARSER, 'Parser values do not match');
-       // $this->assertSame($this->_husky->getTemplatingEngineName(), \Husky\Config::TEMPLATINGENGINE, 'Templating Engines values do not match');
-    }
 
-    /**
-     * @test
-     */
-    public function getTemplateEngineObject_returns_instance_of_expected_class()
-    {
-//        $templateEngine = $this->_husky->getTemplateEngine();
-//        $this->assertInstanceOf('Husky\TemplateEngine\Adapter\IAdapter', $templateEngine, 'TemplateEngine is not of expected Interface');
-//        $this->assertInstanceOf('Husky\TemplateEngine\Adapter\TwigAdapter', $templateEngine, 'TemplateEngine is not of expected concrete class');
-    }
-
-    /**
-     * @test
-     */
-    public function execute()
-    {
-         var_dump($this->_husky->execute());
-    }
 
 
 }
